b98c604f7cde7fc3a03b08e41bb14fbb2e80ec54
René Léveillé <game2world@yahoo.ca>
2018-02-09 16:44:51 -0500

    compile all and place in obj dir

--- tp02/bloc.cpp
+++ tp02/bloc.cpp
@@ -2,9 +2,9 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-void ycrcb2block(cv::Mat &yIn, cv::Mat &crIn, cv::Mat &cbIn, std::vector<Mat> &blocksY, std::vector<Mat> &blocksCr, std::vector<Mat> &blocksCb);
+void ycrcb2block(cv::Mat &yIn, cv::Mat &crIn, cv::Mat &cbIn, std::vector<cv::Mat> &blocksY, std::vector<cv::Mat> &blocksCr, std::vector<cv::Mat> &blocksCb);
 
-void ycrcb2block(cv::Mat &yIn, cv::Mat &crIn, cv::Mat &cbIn, std::vector<Mat> &blocksY, std::vector<Mat> &blocksCr, std::vector<Mat> &blocksCb)
+void ycrcb2block(cv::Mat &yIn, cv::Mat &crIn, cv::Mat &cbIn, std::vector<cv::Mat> &blocksY, std::vector<cv::Mat> &blocksCr, std::vector<cv::Mat> &blocksCb)
 {
 	cv::Size sizeIn = yIn.size();
 	int rowsIn = sizeIn.height;
