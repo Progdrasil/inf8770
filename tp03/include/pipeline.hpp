390022dc7869ffe911d0f09da073750c026ff08d
Thibault <thibault@thibpc.local>
2018-03-29 22:10:22 -0400

    Calcul differences inter-trames + seuillage coupure

--- tp03/include/pipeline.hpp
+++ tp03/include/pipeline.hpp
@@ -3,9 +3,12 @@
 
 #include <boost/filesystem.hpp>
 #include <opencv2/opencv.hpp>
+#include <math.h>
 
 int open(boost::filesystem::path path);
 
-void histogrammes(cv::VideoCapture& vid, std::vector<cv::Mat>* b_hist, std::vector<cv::Mat>* g_hist, std::vector<cv::Mat>* r_hist);
+void histogrammes(cv::VideoCapture& vid, std::vector<cv::Mat>& b_hist, std::vector<cv::Mat>& g_hist, std::vector<cv::Mat>& r_hist);
+float calculerDistance(std::vector<cv::Mat>& b_hist, std::vector<cv::Mat>& g_hist, std::vector<cv::Mat>& r_hist, int indexSrc, int indexCmp);
+void dispImg(std::string message, cv::Mat &image);
 
 #endif
--- tp03/pipeline.cpp
+++ tp03/pipeline.cpp
@@ -3,7 +3,8 @@
 namespace fs = boost::filesystem;
 using namespace std;
 
-const int HIST_SIZE = 256;
+// number of bins of the histograms
+const int HIST_SIZE = 16;
 
 int open(fs::path path){
 	// open the video file
@@ -17,18 +18,38 @@ int open(fs::path path){
 	std::vector<cv::Mat> b_hist, g_hist, r_hist;
 	std::vector<double> dist_b, dist_g, dist_r;
 
-	histogrammes(vid, &b_hist, &g_hist, &r_hist);
+	histogrammes(vid, b_hist, g_hist, r_hist);
+
+
+	int frameNumber = (int) vid.get(CV_CAP_PROP_FRAME_COUNT);
+	double fps = vid.get(CV_CAP_PROP_FPS);
+
+	for(int i=0;i<frameNumber-2;i++)
+	{
+		float dist = calculerDistance(b_hist, g_hist, r_hist, i, i+1);
+		if(dist > 10000)
+		{
+			std::cout << "Coupure detectée entre trames " << i << " et " << i+1 << std::endl;
+			std::cout << "Temps de la vidéo : " << (i+1)/fps << "s" << std::endl;
+			vid.set(1, i);
+			cv::Mat frame;
+			vid >> frame;
+			dispImg("coupure", frame);
+			cv::waitKey(0);
+		}
+	}
 
 	return 0;
 }
 
-void histogrammes(cv::VideoCapture& vid, std::vector<cv::Mat>* b_hist, std::vector<cv::Mat>* g_hist, std::vector<cv::Mat>* r_hist) {
+void histogrammes(cv::VideoCapture& vid, std::vector<cv::Mat>& b_hist, std::vector<cv::Mat>& g_hist, std::vector<cv::Mat>& r_hist) {
 	// Cherche numero de frames dans la video
 	int frameNumber = (int) vid.get(CV_CAP_PROP_FRAME_COUNT);
 	cout << "The number of frames in this file is = " << frameNumber << endl;
 
 	// calcule tout les histogrammes
-	for (int i = 0; i < frameNumber - 1; i++) {
+	for (int i = 0; i < frameNumber - 1 ; i++)
+	{
 		cv::Mat frame;
 		vid >> frame; // Get the next frame from the video
 		if (frame.empty()) {
@@ -40,22 +61,62 @@ void histogrammes(cv::VideoCapture& vid, std::vector<cv::Mat>* b_hist, std::vect
 		std::vector<cv::Mat> bgr_planes;
 		split(frame, bgr_planes);
 
-		float range[] = { 0, HIST_SIZE };
+		// Range of RGB values
+		float range[] = { 0, 256 };
 		const float* histRange = { range };
 
 		bool uniform = true;
 		bool accumulate = false;
 
-		cv:: Mat b_hist_temp, g_hist_temp, r_hist_temp;
+		cv::Mat b_hist_temp, g_hist_temp, r_hist_temp;
 
 		// calcule les histogrammes
 		cv::calcHist(&bgr_planes[0], 1, 0, cv::Mat(), b_hist_temp, 1, &HIST_SIZE, &histRange, uniform, accumulate);
 		cv::calcHist(&bgr_planes[1], 1, 0, cv::Mat(), g_hist_temp, 1, &HIST_SIZE, &histRange, uniform, accumulate);
 		cv::calcHist(&bgr_planes[2], 1, 0, cv::Mat(), r_hist_temp, 1, &HIST_SIZE, &histRange, uniform, accumulate);
 
+		// std::cout << "Number of elements :" << std::endl;
+		// std::cout << frame.rows * frame.cols << std::endl;
+		// std::cout << "Hist elements :" << std::endl;
+		// std::cout << cv::sum( b_hist_temp )[0] << std::endl;
+		// std::cout <<  std::endl;
+
 		// sauvegarde histogrammes
-		b_hist -> push_back(b_hist_temp);
-		g_hist -> push_back(g_hist_temp);
-		r_hist -> push_back(r_hist_temp);
+		b_hist.push_back(b_hist_temp);
+		g_hist.push_back(g_hist_temp);
+		r_hist.push_back(r_hist_temp);
 	}
+}
+
+
+float calculerDistance(std::vector<cv::Mat>& b_hists, std::vector<cv::Mat>& g_hists, std::vector<cv::Mat>& r_hists, int indexSrc, int indexCmp)
+{
+	float distance = 0;
+
+	cv::Mat b_hist_src = b_hists[indexSrc];
+	cv::Mat b_hist_cmp = b_hists[indexCmp];
+
+	cv::Mat g_hist_src = g_hists[indexSrc];
+	cv::Mat g_hist_cmp = g_hists[indexCmp];
+
+	cv::Mat r_hist_src = r_hists[indexSrc];
+	cv::Mat r_hist_cmp = r_hists[indexCmp];
+
+	for(int i = 0; i < b_hists[0].rows * b_hists[0].cols; i++)
+	{
+		distance += (b_hist_src.at<float>(i) - b_hist_cmp.at<float>(i)) * (b_hist_src.at<float>(i) - b_hist_cmp.at<float>(i));
+		distance += (g_hist_src.at<float>(i) - g_hist_cmp.at<float>(i)) * (g_hist_src.at<float>(i) - g_hist_cmp.at<float>(i));
+		distance += (r_hist_src.at<float>(i) - r_hist_cmp.at<float>(i)) * (r_hist_src.at<float>(i) - r_hist_cmp.at<float>(i));
+	}
+	
+	return sqrt(distance);
+}
+
+void dispImg(string message, cv::Mat &image)
+{
+	// Create a window for display.
+	cv::namedWindow(message, cv::WINDOW_AUTOSIZE);
+
+	// Show our image inside it.
+	cv::imshow(message, image);
 }
